beb024d57e310f6627e1dfa86ef7a4d91b7baa4e
leha9321 <leha93.21@gmail.com>
2022-04-22 20:29:26 +0300

    Added pod and configured the keyboard

--- FirebaseApp/LoginViewController.swift
+++ FirebaseApp/LoginViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
-
-
+    
+    @objc func kbDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let kbFrameSife = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + kbFrameSife.height)
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSife.height, right: 0)
+    }
+    
+    @objc func kbDidHide(notification: Notification) {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
 }
 
