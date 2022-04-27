81d4b968776ec0ae2a1efe69d5cf32d3cefe3ca2
leha9321 <leha93.21@gmail.com>
2022-04-27 23:04:04 +0300

    create alertController

--- FirebaseApp/TaskTableViewController.swift
+++ FirebaseApp/TaskTableViewController.swift
@@ -23,6 +23,16 @@ class TaskTableViewController: UITableViewController {
     }
 
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "New Task", message: "Add new task", preferredStyle: .alert)
+        alertController.addTextField()
+        let save = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let textField = alertController.textFields?.first, textField.text != ""
+            else { return }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(save)
+        alertController.addAction(cancel)
+        present(alertController, animated: true)
     }
     
     @IBAction func signOutTapped(_ sender: UIBarButtonItem) {
